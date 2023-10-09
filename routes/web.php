eb7ec5e9f9b7c0de46fe226149cbfd3fadd7a190
mamann12 <rizkyrahmand@gmail.com>
2023-10-09 13:08:03 +0700

    New Update

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/profil' , function(){
+    echo "Namaku Ivan";
+    return view('profile');
+});
+
+Route::get('/mahasiswa/{nama?}' , function($nama = "Ivan"){
+    echo "Namaku $nama";
+});
+
+Route::get('/mahasiswa/{nama?}/{kelas?}' , function($nama = "Ivan", $kelas ="if3a"){
+    echo "<h1>Namaku $nama dari $kelas</h1>";
+});
+
+Route::get('/hubungi/{telp?}' , function($telp = "kamu nobob"){
+    echo "<h1> Hubungi kami $telp </h1>";
+});
+
+Route::redirect("/contact" , '/hubungi')->name("call");
+
+Route::get("/halo" , function(){
+    echo "<a href='".route('call') . "'>" . route('call')."</a>";
+});
+
+Route::prefix("/dosen")->group(function(){
+    Route::get("/jadwal" , function(){
+        echo "Jadwal Dosen";
+    });
+    Route::get("/materi" , function(){
+        echo "Materi Dosen";
+    });
+});
